9fa4fc16e5511c8e416bb64662bf77e3ae8a04cd
Stacey Markiv <stasy-9876@mail.ru>
2022-04-09 11:15:15 +0300

    Merge branch 'master' of https://github.com/StaceyMarkiv/Currency_converter

--- currency_converter.py
+++ currency_converter.py
@@ -2,6 +2,10 @@ import os
 import urllib.request
 from xml.dom import minidom
 
+input_1 = input('Введите первую валюту и сумму для конвертации: ')
+input_2 = input('Введите вторую валюту: ')
+input_1 = input_1.split()
+
 url = "http://www.cbr.ru/scripts/XML_daily.asp"
 
 # Чтение URL
@@ -21,38 +25,33 @@ date = "\nКонвертация по курсу валют ЦБ РФ на \x1b[
 
 # Извлечение данных по валютам
 currency = doc.getElementsByTagName("Valute")
-
-NOK = []  # Данные по норвежским кронам
-HUF = []  # Данные по венгерским форинтам
+curr_1_data = []
+curr_2_data = []
 for rate in currency:
     charcode = rate.getElementsByTagName("CharCode")[0]
     name = rate.getElementsByTagName("Name")[0]
     value = rate.getElementsByTagName("Value")[0]
     nominal = rate.getElementsByTagName("Nominal")[0]
-    if charcode.firstChild.data == 'NOK':
-        NOK = [name.firstChild.data, value.firstChild.data, nominal.firstChild.data]
-    elif charcode.firstChild.data == 'HUF':
-        HUF = [name.firstChild.data, value.firstChild.data, nominal.firstChild.data]
-
-NOK[1] = NOK[1].replace(',', '.')
-HUF[1] = HUF[1].replace(',', '.')
+    if charcode.firstChild.data == input_1[0]:
+        curr_1_data = [name.firstChild.data, value.firstChild.data, nominal.firstChild.data]
+        curr_1_data[1] = curr_1_data[1].replace(',', '.')
+    elif charcode.firstChild.data == input_2:
+        curr_2_data = [name.firstChild.data, value.firstChild.data, nominal.firstChild.data]
+        curr_2_data[1] = curr_2_data[1].replace(',', '.')
 
 # Конвертация валют
-
-# NOK_to_RUB = float(NOK[1]) / int(NOK[2])
-# RUB_to_HUF = int(HUF[2]) / float(HUF[1])
-# NOK_to_HUF = NOK_to_RUB * RUB_to_HUF
-#
-# print(date)
-# print(f'1 {NOK[0]} = {round(NOK_to_HUF, 2)} {HUF[0]}')
-
-HUF_to_RUB = float(HUF[1]) / int(HUF[2])
-RUB_to_NOK = int(NOK[2]) / float(NOK[1])
-
-convert_sum = float(input('Введите сумму в венгерских форинтах: '))
-result = convert_sum * HUF_to_RUB * RUB_to_NOK
+if input_1[0] == 'RUB':
+    curr_1_data = ['Рубль', '1', '1']
+    result = float(input_1[1]) * (int(curr_2_data[2]) / float(curr_2_data[1]))
+elif input_2 == 'RUB':
+    curr_2_data = ['Рубль', '1', '1']
+    result = float(input_1[1]) * (float(curr_1_data[1]) / int(curr_1_data[2]))
+else:
+    first_to_RUB = float(curr_1_data[1]) / int(curr_1_data[2])
+    RUB_to_second = int(curr_2_data[2]) / float(curr_2_data[1])
+    result = float(input_1[1]) * first_to_RUB * RUB_to_second
 
 print(date)
-print(f'{convert_sum} венгерских форинтов = {round(result, 2)} норвежских крон')
+print(f'{input_1[1]} {curr_1_data[0]} = {round(result, 2)} {curr_2_data[0]}')
 
 os.remove('currency_data.xml')
