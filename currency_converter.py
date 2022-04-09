c53741cb3b98ab0c14d4ec13407b62c04f07cea1
Stacey Markiv <stasy-9876@mail.ru>
2022-04-10 02:04:33 +0300

    Merge branch 'master' of https://github.com/StaceyMarkiv/Currency_converter

--- currency_converter.py
+++ currency_converter.py
@@ -5,6 +5,8 @@ from xml.dom import minidom
 input_1 = input('Введите первую валюту и сумму для конвертации: ')
 input_2 = input('Введите вторую валюту: ')
 input_1 = input_1.split()
+amount = float(input_1[1])
+input_1 = input_1[0]
 
 url = "http://www.cbr.ru/scripts/XML_daily.asp"
 
@@ -32,26 +34,33 @@ for rate in currency:
     name = rate.getElementsByTagName("Name")[0]
     value = rate.getElementsByTagName("Value")[0]
     nominal = rate.getElementsByTagName("Nominal")[0]
-    if charcode.firstChild.data == input_1[0]:
+    if charcode.firstChild.data == input_1:
         curr_1_data = [name.firstChild.data, value.firstChild.data, nominal.firstChild.data]
         curr_1_data[1] = curr_1_data[1].replace(',', '.')
+        if input_2 == input_1:
+            curr_2_data = curr_1_data
     elif charcode.firstChild.data == input_2:
         curr_2_data = [name.firstChild.data, value.firstChild.data, nominal.firstChild.data]
         curr_2_data[1] = curr_2_data[1].replace(',', '.')
 
 # Конвертация валют
-if input_1[0] == 'RUB':
-    curr_1_data = ['Рубль', '1', '1']
-    result = float(input_1[1]) * (int(curr_2_data[2]) / float(curr_2_data[1]))
+if input_1 == input_2:
+    if input_1 == 'RUB':
+        curr_1_data = ['Рубль']
+        curr_2_data = ['Рубль']
+    result = amount
+elif input_1 == 'RUB':
+    curr_1_data = ['Рубль']
+    result = amount * (int(curr_2_data[2]) / float(curr_2_data[1]))
 elif input_2 == 'RUB':
-    curr_2_data = ['Рубль', '1', '1']
-    result = float(input_1[1]) * (float(curr_1_data[1]) / int(curr_1_data[2]))
+    curr_2_data = ['Рубль']
+    result = amount * (float(curr_1_data[1]) / int(curr_1_data[2]))
 else:
     first_to_RUB = float(curr_1_data[1]) / int(curr_1_data[2])
     RUB_to_second = int(curr_2_data[2]) / float(curr_2_data[1])
-    result = float(input_1[1]) * first_to_RUB * RUB_to_second
+    result = amount * first_to_RUB * RUB_to_second
 
 print(date)
-print(f'{input_1[1]} {curr_1_data[0]} = {round(result, 2)} {curr_2_data[0]}')
+print(f'{amount} {curr_1_data[0]} = {round(result, 2)} {curr_2_data[0]}')
 
 os.remove('currency_data.xml')
